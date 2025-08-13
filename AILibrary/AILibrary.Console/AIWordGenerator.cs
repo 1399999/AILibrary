747c8026e65bfbd2c51b6cdc1beb6b938c854ddf
1399999 <mzhebrunov@gmail.com>
2025-08-12 20:09:58 -0400

    Adds unfinished code.

---
+++ AILibrary/AILibrary.Console/AIWordGenerator.cs
@@ -0,0 +1,98 @@
+﻿namespace AILibrary;
+
+public static class AIWordGenerator
+{
+    static Dictionary<char, int> alphabetNumbers = new(); // stoi
+    static int blockSize = 3;
+
+    public static void GenerateWord()
+    {
+        for (int i = 0; i < SystemModel.Alphabet.Length; i++)
+        {
+            alphabetNumbers.Add(SystemModel.Alphabet[i], i);
+        }
+
+        string[] words = File.ReadAllLines("C:\\AITrainingSets\\Names.txt");
+
+        List<int> allWordsTemp = new List<int>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words[i].Length; j++)
+            {
+                allWordsTemp.Add(alphabetNumbers[words[i][j]]);
+            }
+
+            allWordsTemp.Add(0);
+        }
+
+        int[] allWords = allWordsTemp.ToArray(); // Y, Dimensions: <all words>
+        int[][] blockSizeWords = new int[allWords.Length][]; // X, Dimensions: <all words>x<block size>
+
+        // Building the dataset
+
+        int l = 0;
+
+        for (int i = 0; i < words.Length && i < 5; i++)
+        {
+            Console.WriteLine(words[i]);
+
+            string word = string.Empty;
+
+            for (int j = 0; j < blockSize; j++)
+            {
+                word += SystemModel.Alphabet[0];
+            }
+
+            word += words[i];
+            word += SystemModel.Alphabet[0];
+
+            for (int j = 0; j < words[i].Length + 1; j++, l++)
+            {
+                blockSizeWords[l] = new int[blockSize];
+
+                for (int k = 0; k < blockSize; k++)
+                {
+                    Console.Write(word[j + k]);
+                    blockSizeWords[l][k] = alphabetNumbers[word[j + k]];
+                }
+
+                Console.Write(" ---> ");
+                Console.WriteLine(word[j + blockSize]);
+            }
+        }
+
+
+    }
+
+    public static double[] CreateRandomNeurons(int x, bool useSeed, int seed = 0)
+    {
+        NormalRandom rng = useSeed ? new NormalRandom(seed) : new NormalRandom();
+
+        double[] output = new double[x];
+
+        for (int i = 0; i < x; i++)
+        {
+            output[i] = rng.NextStandardNormal();
+        }
+
+        return output;
+    }
+
+    public static double[][] CreateRandomNeurons(int x, int y, bool useSeed, int seed = 0)
+    {
+        NormalRandom rng = useSeed ? new NormalRandom(seed) : new NormalRandom();
+
+        double[][] output = new double[x][];
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                output[i][j] = rng.NextStandardNormal();
+            }
+        }
+
+        return output;
+    }
+}
---
+++ AILibrary/AILibrary.Console/SystemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AILibrary;
+
+public static class SystemModel
+{
+    public static char[] Alphabet = new char[27] // itos
+    {
+        '.', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+}
