00627fd05ce579696d1986c2dba6393c1d218b15
1399999 <mzhebrunov@gmail.com>
2025-08-16 15:27:41 -0400

    Adds the Div class.

--- AILibrary/AILibrary.Console/Temp/TensorOperations.cs
+++ AILibrary/AILibrary.Console/Temp/TensorOperations.cs
@@ -520,6 +520,91 @@ public class Tensor
             }
         }
     }
+
+    private class Div()
+    {
+        public List<Tensor> Parents { get; set; } = new List<Tensor>();
+        public List<Tensor> Cache { get; set; } = new List<Tensor>();
+
+        public Tensor Forward(Tensor a, Tensor b)
+        {
+            bool requiresGrad = a.RequiresGrad || b.RequiresGrad;
+
+            // Get new Tensors's data:
+            object data = a._data / b._data;
+
+            // Create new Tensor's data:
+            Tensor z = new Tensor(data, requiresGrad, operation: new Div());
+
+            // Add new Tensors to "children" and old Tensors to "parents":
+            Parents.Add(a);
+            Parents.Add(b);
+            a.Children.Add(z);
+            b.Children.Add(z);
+            Cache.Add(a);
+            Cache.Add(b);
+
+            return z;
+        }
+
+        public void Backward(Tensor dz, Tensor z)
+        {
+            Tensor a = Cache[0];
+            Tensor b = Cache[1];
+
+            // Find gradients relative to "a", and pass it downstream:
+            if (a.RequiresGrad)
+            {
+                // d/da(a*b) = b, apply chain rule:
+                var da = dz * (1 / b._data);
+
+                // Rescale gradient to have the same shape "a":
+                int gradDim = dz.Shape.Count;
+                int inDim = a.Shape.Count;
+
+                for (int i = 0; i < gradDim - inDim; i++)
+                {
+                    da = da.Sum(dim: 0);
+                }
+
+                for (int n = 0; n < a.Shape.Count; n++)
+                {
+                    if (a.Shape[n] == 1)
+                    {
+                        da = da.Sum(dim: n, keepDims: true);
+                    }
+                }
+
+                a.Backward(da, z);
+            }
+
+            // Find gradients relative to "b", and pass it downstream:
+            if (b.RequiresGrad)
+            {
+                // d/da(a*b) = a, apply chain rule:
+                var db = -dz * a._data / (b._data ^ 2);
+
+                // Rescale gradient to have the same shape "a":
+                int gradDim = dz.Shape.Count;
+                int inDim = b.Shape.Count;
+
+                for (int i = 0; i < gradDim - inDim; i++)
+                {
+                    db = db.Sum(dim: 0);
+                }
+
+                for (int n = 0; n < b.Shape.Count; n++)
+                {
+                    if (b.Shape[n] == 1)
+                    {
+                        db = db.Sum(dim: n, keepDims: true);
+                    }
+                }
+
+                b.Backward(db, z);
+            }
+        }
+    }
 }
 
 class Parameter : Tensor
